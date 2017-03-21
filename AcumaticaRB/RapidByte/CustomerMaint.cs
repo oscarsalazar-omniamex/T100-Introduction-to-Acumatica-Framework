9dd24eb3c18e38aaf8bd940b55af0c526758d7f1
osalazar <osalazar@DESARROLLO3>
2017-03-21 11:55:42 -0600

    Objetos Creados

---
+++ AcumaticaRB/RapidByte/CustomerMaint.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PX.SM;
+using PX.Data;
+
+
+namespace AcumaticaRB.RapidByte
+{
+    public class CustomerMaint : PXGraph<CustomerMaint>
+    {
+    }
+}
